e55c53772c254fc774e4bb8a6f318b52ea6bd82f
Carlos Alves <carlos.alves@newnote.pt>
2026-05-10 17:44:59 +0100

    Updated Home Screen Logic to support Avatar Screen

--- app/src/main/java/com/carlosalves/emojitar/homescreen/HomeUiState.kt
+++ app/src/main/java/com/carlosalves/emojitar/homescreen/HomeUiState.kt
@@ -5,5 +5,6 @@ import com.carlosalves.emojitar.model.Emoji
 data class HomeUiState(
     val currentEmoji: Emoji? = null,
     val isLoading: Boolean = false,
-    val emojiListPopulated: Boolean = false
+    val emojiListPopulated: Boolean = false,
+    val avatarsLoaded: Boolean = false
 )
